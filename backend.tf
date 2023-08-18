5008a12ffe9fc6c15e944e2b799895ada6d0e05b
chandan <chandanabbupur@gmail.com>
2023-08-18 12:42:56 +0530

    done

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 # --- root/backend.tf ---
-
 terraform {
   backend "s3" {
-    bucket = "jenkins-bucket43982789"
+    bucket = "ckc-jenkins-aws"
     key    = "remote.tfstate"
     region = "us-east-1"
   }
+
 }
